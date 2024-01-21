e4305de20674492e89d1138a96c776cb1a9c8661
Raphael Medeiros <medeiros.ph@gmail.com>
2024-01-20 23:00:55 -0300

    Update Mobile func

--- js/main.js
+++ js/main.js
@@ -138,3 +138,19 @@ function deferVideo() {
 
     }
     window.onload = deferVideo;
+
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+}
+)
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
